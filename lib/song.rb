4f5af226de8a8fb0e28a8539b1b6912bb98c2a69
Stef Claus <stef.claus@gmail.com>
2018-10-29 02:17:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,7 @@ class Song
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
+    genre = Song.new(song_info[2])
     song.artist_name = song_info[0]
     song
   end 
